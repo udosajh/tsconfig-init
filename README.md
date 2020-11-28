e88f7cb2629c391c49135dc69a3d97899f14ed1d
uday dosajh <dosajhuday@gmail.com>
2020-11-28 16:55:16 +0530

    tsconfig-init: create three files config files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tsconfig-init
-Create ts config files in the current directory
+- Create ts config files in the current directory
+
+
+## command
+    - run this command in terminal
+        - npx tsconfig-init
+            - this will create three files (tsconfig.src.json, tsconfig.test.json and tsconfig.base.json) in your current directory
+            - current directory => directory opened in terminal
+        - npx is available >= npm version (5.2.0)
+            - you can install npx if your npm version is below 5.2.
+                - npm i npx
+
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import fs = require("fs");
+import shelljs = require("shelljs");
+import path = require("path");
+import jsBeautify = require("js-beautify");
+const currentDirPath = shelljs.pwd().stdout;
+
+const tsconfigBase = {
+    "compileOnSave": true,
+    "compilerOptions": {
+        "target": "es2015",
+        "module": "commonjs",
+        "lib": ["ES2015", "DOM"],
+        "declaration": true,
+        "sourceMap": true,
+        "removeComments": true,
+        "downlevelIteration": true,
+        "strict": true,
+        "noImplicitAny": true,
+        "strictNullChecks": true,
+        "noImplicitThis": true,
+        "alwaysStrict": true,
+        "noUnusedLocals": true,
+        "noUnusedParameters": true,
+        "noImplicitReturns": true,
+        "noFallthroughCasesInSwitch": true,
+        "moduleResolution": "node",
+        "baseUrl": ".",
+        "paths": {
+            "*": [
+                "node_modules/*"
+            ]
+        },
+        "rootDirs": [
+            "."
+        ]
+    }
+};
+
+const tsconfigSrc = {
+    "extends": "./tsconfig.base.json",
+    "compilerOptions": {
+        "outDir": "./dist",
+    },
+    "include": [
+        "src/**/*",
+    ]
+};
+
+const tsconfigTest = {
+    "extends": "./tsconfig.base.json",
+    "compilerOptions": {
+        "outDir": "./dist",
+    },
+    "include": [
+        "test/**/*"
+    ]
+};
+
+const tsconfigBaseFilePath = currentDirPath + path.sep + "tsconfig.base.json";
+const tsconfigSrcFilePath = currentDirPath + path.sep + "tsconfig.src.json";
+const tsconfigTestFilePath =       currentDirPath + path.sep + "tsconfig.test.json";
+
+
+fs.writeFileSync(tsconfigBaseFilePath, jsBeautify.js_beautify(JSON.stringify(tsconfigBase)));
+fs.writeFileSync(tsconfigSrcFilePath, jsBeautify.js_beautify(JSON.stringify(tsconfigSrc)));
+fs.writeFileSync(tsconfigTestFilePath, jsBeautify.js_beautify(JSON.stringify(tsconfigTest)));
